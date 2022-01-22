f7828500f7625a61bd822bd22a47d722c0f351c9
chinmay <chinmaymhatre12@gmail.com>
2022-01-22 16:43:55 +0530

    Updates

---
+++ precache-manifest.9be0d8c011691db739b59173d57e6051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d19070a47440622ffd5a0b1ffd6e1f",
+    "url": "/currency_detector/index.html"
+  },
+  {
+    "revision": "be204e9c4eaa563ccbbc",
+    "url": "/currency_detector/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "97c69704df15ac439f13",
+    "url": "/currency_detector/static/js/2.70716aea.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/currency_detector/static/js/2.70716aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be204e9c4eaa563ccbbc",
+    "url": "/currency_detector/static/js/main.9a127143.chunk.js"
+  },
+  {
+    "revision": "c09cbe3d901ad71d54f0",
+    "url": "/currency_detector/static/js/runtime-main.919e6ea8.js"
+  }
+]);
